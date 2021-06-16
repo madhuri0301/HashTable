9f81b8a4fd2bc2fc80f1495ec4e5640f6a81ca69
Madhuri <harshupatil0301@gmail>
2021-06-16 23:04:02 +0530

    [Madhuri]Add : Welcome Message Uploaded

---
+++ HashTableAndBinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableAndBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The hash Table And Binary Search Tree Programs");
+        }
+    }
+}
