85845ea60698d27f8f55a08a95e5efaeb9e5ce62
Madhuri <harshupatil0301@gmail>
2021-06-18 14:49:26 +0530

    [Madhuri]Add : Method To Find Frequency Of Words From Sentence

--- HashTableAndBinarySearchTree/Program.cs
+++ HashTableAndBinarySearchTree/Program.cs
@@ -6,7 +6,16 @@ namespace HashTableAndBinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The hash Table And Binary Search Tree Programs");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            Operation operation = new Operation();
+            operation.Frequency(hash);
         }
     }
 }
+
